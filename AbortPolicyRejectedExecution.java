53a6f3ded6390aa5c1b442e6002f5f190f5c3db2
Adan Urban Reyes <aurban@aeromexico.com>
2021-04-20 10:29:10 -0500

    AbortPolicyRejectedExecution

---
+++ AbortPolicyRejectedExecution.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.*;
+
+public class AbortPolicyRejectedExecution {
+
+    public static final byte NUMBER_OF_TASK = 3;
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Starting to run the task on thread " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+                System.out.println("Ending of run the task on thread " + Thread.currentThread().getName());
+            } catch (InterruptedException ie) {
+                System.out.println("Ending; with exception of run the task on thread " + Thread.currentThread().getName());
+            }
+        }
+
+    }
+
+    /**
+     * Create a Pool using a queue with just one slot, so if we send more than one task
+     * at the same time we will have rejections in this case that rejection will be
+     * handler first by AbortPolicy class which will propagate the exeption until
+     * the parent method who call execute method.
+     *
+     * @param args
+     */
+    public static void main(String... args) {
+        System.out.println(String.format("Starting %s thread", Thread.currentThread().getName()));
+        ExecutorService es = new ThreadPoolExecutor(1
+                , 1
+                , 120
+                , TimeUnit.SECONDS
+                , new ArrayBlockingQueue<>(1)
+                , new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < NUMBER_OF_TASK; i++) {
+            try {
+                es.execute(new Task());
+            } catch (RejectedExecutionException ree) {
+                System.out.println(ree);
+            }
+        }
+        System.out.println(String.format("Ending %s thread", Thread.currentThread().getName()));
+    }
+
+}
